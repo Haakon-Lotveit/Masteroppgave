82e457108eaba3c17ba970e30b52cb793a253e4b
Haakon Løtveit <haakon_lotveit@live.no>
2014-10-16 14:03:23 +0200

    Kan nå håndtere uordnede lister, med nøsting.

--- markdown-compiler/markdown.lisp
+++ markdown-compiler/markdown.lisp
@@ -32,6 +32,8 @@
 (ql:quickload :split-sequence)
 (load "../front-end-compiler/auxiliary-functions.lisp") ; Vi vil ha make-hash-table-from-list funksjonen.
 
+(import 'CL-PPCRE:SCAN)
+(import 'CL-PPCRE:REGEX-REPLACE)
 					; Testdata
 (defvar *test-string-large*
 "This is not a normal test string.
@@ -48,6 +50,7 @@ And block quotes too:
     And this concludes the quote.
 
 ------------------------------------
+
 And that was a line!
 
 Not to be outdone, here's a list:
@@ -64,7 +67,144 @@ And that's about all for now. I should add in some extras, such as:
   5. citations (an extension to the markdown language. I'm thinking about using ¤ (currency char) or § (pragraph char) for citations.")
 
 (defparameter *current-subtest* "Here is some _underlined_ words. This _is_ _important_!")
+(defparameter *markdown-indentation-level* 0)
+(defparameter *markdown-indentation-step* 4)
 
+(defun increase-markdown-indentation ()
+  (incf *markdown-indentation-level* *markdown-indentation-step*))
+(defun decrease-markdown-indentation ()
+  (decf *markdown-indentation-level* *markdown-indentation-step*))
+(defun reset-markdown-indentation-level ()
+  (setf *markdown-indentation-level* 0))
+
+(defun split-string-by-newlines (string)
+  (SPLIT-SEQUENCE:SPLIT-SEQUENCE #\Newline string))
+
+(defparameter *lister*
+  " Her kommer det lister:
+  * Dette er en liste uten orden.
+  * Her kommer det et element til.
+  * Listen er over når det kommer en tom linje.
+  * En tom linje er en linje med bare whitespace.
+
+  * Dette er en ny liste.
+    - Du kan blande listetegn om du vil.
+    - Men dette er en kjedet liste, eller en liste i en liste.
+  - Men lister defineres per indentering.
+
+Nå er listene ferdig begge to.")
+
+(defun prettyprint (stream object)
+  (dotimes (i *markdown-indentation-level*)
+    (format stream " "))
+
+  (format stream "~A" object))
+
+;; Hjelpeklasse
+(defclass list-status ()
+  ((in-list
+    :accessor in-list
+    :initform nil
+    :initarg :in-list)
+   (indentation-spaces
+    :accessor indentation-spaces
+    :initform '(0)
+    :initarg :indentation-spaces)))
+
+(defgeneric increase-indentation (status number))
+(defmethod increase-indentation ((status list-status) number)
+  (setf (indentation-spaces status)
+	(+ (indentation-spaces status) number))
+  status)
+
+(defgeneric push-list-indent-level (status new-level))
+(defmethod push-list-indent-level ((status list-status) new-level)
+  (setf (indentation-spaces status)
+	(cons new-level (indentation-spaces status)))
+  status)
+
+(defgeneric pop-list-indent-level (status))
+(defmethod pop-list-indent-level ((status list-status))
+  (pop (indentation-spaces status)))
+
+(defun open-new-unordered-list (status line stream)
+  (prettyprint-line stream "(UNORDERED-LIST")
+  (update-indentation-spaces status line)
+  (setf (in-list status) T)
+  (increase-markdown-indentation))
+  
+(defun close-list (status stream)
+    (format stream ")")
+    (pop-list-indent-level status)
+    (when (= (first (indentation-spaces status)) 0)
+      (setf (in-list status) nil))
+    (decrease-markdown-indentation))
+
+(defgeneric currently-indented-spaces (status))
+(defmethod currently-indented-spaces ((status list-status))
+  (first (indentation-spaces status)))
+
+(defmethod print-object ((status list-status) stream)
+  (format stream "#<status-list in-list: ~A indentation-spaces: ~>" (in-list status) (indentation-spaces status)))
+
+
+(defun prettyprint-line (stream object)
+  (format stream "~%")
+
+  (prettyprint stream object))
+
+(defun remove-list-stuff (string)
+  (regex-replace "\\A\\s*(\\*|-)\\s*" string ""))
+
+(defun count-spaces (string)
+  (second (multiple-value-list (scan " *" (CL-PPCRE:regex-replace-all "\\t" string "    ")))))
+
+(defun add-line-item (line stream)
+  (let ((line-item (format 'nil "(LINE-ITEM ~A)" (prin1-to-string (remove-list-stuff line)))))
+    (prettyprint-line stream line-item)))
+
+(defun update-indentation-spaces (status line)
+  (push-list-indent-level status (count-spaces line)))
+
+(defun deal-with-list (list-status output-stream line)
+  (if (in-list list-status)
+      (cond
+	((> (count-spaces line) (currently-indented-spaces list-status))
+	 (open-new-unordered-list list-status line output-stream))
+	((< (count-spaces line) (currently-indented-spaces list-status))
+	 (close-list list-status output-stream)))
+      (progn
+	(open-new-unordered-list list-status line output-stream)))
+  (add-line-item line output-stream))
+
+(defun deal-with-no-list (list-status output-stream line)
+  "Lukker listen, setter status til å ikke være i en liste, og senker indenteringsnivået ett hakk."
+  (when (in-list list-status)
+    (close-list list-status output-stream))
+  (prettyprint-line output-stream line))
+
+
+(defun interpret-unordered-lists (input-string)
+  (reset-markdown-indentation-level)
+  (let ((output-string (make-growable-string))
+	(status (make-instance 'list-status))
+	(current-line 1))
+    (with-output-to-string (stream output-string)
+      ;; Explanation of regex:
+      ;; Start of string, or post-newline, some-or none whitespace, a * or a -,
+      ;; and then some-or-none whitespace
+      (let* ((find-unordered-lists-regexp "(^|\\n)\\s*(\\*|-)\\s*") 
+	     (lines (split-string-by-newlines input-string)))
+	(loop for line in lines do
+	       (if (scan find-lists-regexp line)
+		   (deal-with-list status stream line)
+		   (deal-with-no-list status stream line))
+	     (incf current-line))))
+    output-string))
+
+;; This is bullshit, and should be refactored to a more user-friendly expression of love.
+;; Better idea would be to have functions for everything that could be done, and then just chain those funcalls together.
+;; But this stuff is staying for now, until I have something better, just so I have something to look at.
 (defun compile-string (string output-stream)
   (format output-stream "~%(text :markdown-standard~%")
 
@@ -79,8 +219,11 @@ And that's about all for now. I should add in some extras, such as:
 
   (format output-stream ")"))
 
+;;TODO: Refactor this to be a part of compile-string.
 (defun compile-line (line output-stream line-number)
   (let* ((output-string (make-growable-string))
+	 ;;TODO: Refactor this into something better.
+	 
 	 (special-tokens (make-hash-table-from-list
 			  '((#\* "BOLD")
 			    (#\_ "UNDERLINE")
@@ -109,12 +252,23 @@ And that's about all for now. I should add in some extras, such as:
 						      (format stream "(CURSIVE "))
 						  (setf (gethash "CURSIVE" special-states) (not state))))))))
     (with-output-to-string (stream output-string)
-      (loop for char across line do
-	   (let ((lookup (gethash char special-tokens)))
-	     (if lookup
-		 (let ((state (gethash lookup special-states)))
-		   (funcall (gethash lookup syntactic-functions) state stream))
-		 (format stream "~A" char)))))
+      ;; Check for special rules, such as whole-lines etc, that terminates parsing of the line.
+      (cond
+      ;; If a line is composed of only zero or more characters of whitespace, 
+      ;; then at least four dashes (-), and then zero or more characters of whitespace,
+      ;; then the entire line should be interpreted as: "(vertical-space)".
+	((let ((match (cl-ppcre:scan "\\s*----+\\s*" line)))
+	   (and (not (null match))
+		(= 0 match)))
+	 (format stream "(~A)~%" "HORIZONTAL-LINE"))
+	('otherwise
+	 ;; If no terminating special rules have been found, check the generic rules.
+	 (loop for char across line do
+	      (let ((lookup (gethash char special-tokens)))
+		(if lookup
+		    (let ((state (gethash lookup special-states)))
+		      (funcall (gethash lookup syntactic-functions) state stream))
+		    (format stream "~A" char)))))))
 
     ;;After we've printed everything to the string, let's print the string where it's supposed to go:
     (format output-stream output-string)
@@ -130,3 +284,6 @@ And that's about all for now. I should add in some extras, such as:
 (compile-string *current-subtest* *standard-output*)
 (compile-string *test-string-large* *standard-output*)
 
+	
+
+	
