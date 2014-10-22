e9e33fa65d7ba0a85cb1f9cae60a64bad3507f0b
Haakon Løtveit <haakon_lotveit@live.no>
2014-10-22 17:18:10 +0200

    Glemte å legge til Main.lisp

---
+++ Main.lisp
@@ -0,0 +1,5 @@
+;; Loads and runs the program so far.
+(load (merge-pathnames "aux/auxilliary-functions.lisp" *default-pathname-defaults*))
+(load (merge-pathnames "front-end-compiler/parser.lisp" *default-pathname-defaults*))
+(load (merge-pathnames "front-end-compiler/unit-test-functions.lisp" *default-pathname-defaults*))
+(load (merge-pathnames "front-end-compiler/unit-tests.lisp" *default-pathname-defaults*))
